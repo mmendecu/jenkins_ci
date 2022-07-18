d146522aa536c9f3053f1a3723ce3ea1189fdf5c
root <root@SVQ-CM3JTG3.usersad.everis.int>
2022-07-18 13:32:49 +0200

    vuelvo a meter rama con todo

--- billing/src/main/java/com/paymentchain/billing/InvoiceApplication.java
+++ billing/src/main/java/com/paymentchain/billing/InvoiceApplication.java
@@ -32,7 +32,6 @@ public class InvoiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InvoiceApplication.class, args);
-        System.out.println("Mensaje de prueba de automatizacion push en pipeline");
     }
 
      //Define all details for app info
